b975d2e3a639d790c52e8666fc4680c9f0ed2bde
TommyPigna <tommasopignatta@gmail.com>
2025-01-09 11:38:58 +0100

    Aggiunta del javadoc e dei relativi commenti per le classi TestGiocoSedie e Posto

--- src/Posto.java
+++ src/Posto.java
@@ -4,23 +4,38 @@
  */
 
 /**
- *
+ * Classe che rappresenta un posto, ovvero una sedia.
+ * 
  * @author tommaso pignatta
  */
 class Posto
 
 {
-	private boolean occupato;
-
+	private boolean occupato; // Indica se la sedia è occupata
+        /**
+         * Costruttore che inizializza il posto come non occupato.
+         */
 	public Posto()
 
 	{
 		occupato = false;
 	}
+        
+        /**
+         * Controlla se il posto è libero.
+         * 
+         * @return true se è il posto è libero, altrimenti false 
+         */
 
 	public synchronized boolean libero() {
 		return (!occupato);
 	}
+        
+        /**
+         * Tenta di occupare il posto.
+         * 
+         * @return true se l'occupazione ha successo, altrimenti false se il posto è già stato occupato. 
+         */
 
 	public synchronized boolean occupa() {
 		if (occupato)
--- src/TestGiocoSedie.java
+++ src/TestGiocoSedie.java
@@ -6,27 +6,36 @@
 import java.util.logging.Logger;
 
 /**
- *
+ * Classe principale per simulare il gioco delle sedie.
+ * Il programma crea un array di sedie (Posto) e partecipanti, simulando così un gioco
+ * in cui i partecipanti cercano di occupare una tra le varie sedie disponibili.
+ * Una sedia è rappresentata dalla classe {@link Posto}, mentre i partecipanti vengono gestiti come thread.
+ * Un display, invece, visualizza lo stato delle sedie.
+ * 
  * @author tommaso pignatta
  */
 public class TestGiocoSedie {
-    private final static int NUMSEDIE = 15;
+    private final static int NUMSEDIE = 15; //Numero di sedie disponibili
     private static Logger logger = Logger.getLogger("GiocoSedie.TestGiocoSedie");
     
     /**
+     * Metodo main del programma.
+     * Crea un array di sedie e partecipanti, avvia i thread corrispondenti e simula il gioco delle sedie.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Creazione dell'array di sedie
         Posto sedie[] = new Posto[NUMSEDIE];
 
 	for (int k = 0; k < sedie.length; k++)
 		sedie[k] = new Posto();
-
+        // Creazione e avvio del display
 	Display display = new Display(sedie);
 	//System.out.println("Sto facendo partire il Display.");
         logger.info("Sto facendo partire il Display.\n");
 	display.start();
-
+        // Creazione e avvio dell'array dei partecipanti
 	Partecipante array[] = new Partecipante[NUMSEDIE+1];
 	for (int i = 0; i < NUMSEDIE + 1; i++) {
 		array[i] = new Partecipante(sedie);
